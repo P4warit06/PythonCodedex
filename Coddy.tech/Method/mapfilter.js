8e4db65011db290a67ec56c502a134dfd8e8aab6
P4warit <142296227+P4warit06@users.noreply.github.com>
2025-07-07 20:24:27 +0700

    Create mapfilter.js

---
+++ Coddy.tech/Method/mapfilter.js
@@ -0,0 +1,11 @@
+// You are given an array numbers = [1, 2, 3, 4, 5]. 
+// Perform the following steps and print the results directly:
+// Use map() to create a new array where each number is multiplied by 3, and print the result.
+// Use filter() to create a new array that only includes numbers greater than 3, and print the result.
+let numbers = [1, 2, 3, 4, 5];
+let multipliedByThree = numbers.map(num => num * 3);
+let filteredNumbers = numbers.filter(num => num > 3);
+console.log(multipliedByThree);
+console.log(filteredNumbers);
+
+
