f9d41cc5cace9f1e77723a8bcca2c1c3d7c373f7
P4warit <142296227+P4warit06@users.noreply.github.com>
2025-08-04 09:38:12 +0700

    add file

--- Coddy.tech/js/pyramid.js
+++
@@ -1,8 +0,0 @@
-let  n =5 // Don't change this line
-let rows = parseInt(n / 2) + 1;
-for (let i = 0; i < rows; i++) {
-  let stars = "";
-  stars += "*".repeat(2 * (i + 1) - 1);
-  console.log(stars);
-}
- 
